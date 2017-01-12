a715a69ec38c0363e3f45965dd8d0e0ab9cfddc4
Tapio Vaattanen <tvaattanen@loyalty.com>
2017-01-12 15:50:57 -0500

    Changed idx name for for loop (i --> idx).

--- src/Sorting.java
+++ src/Sorting.java
@@ -6,9 +6,9 @@ public class Sorting {
     public static int[] sort(int[] arr) {
         int x;
         int j;
-        for (int i = 1 ; i < arr.length ; i++) {
-            x = arr[i];
-            j = i - 1;
+        for (int idx = 1 ; idx < arr.length ; idx++) {
+            x = arr[idx];
+            j = idx - 1;
             while (j >= 0 && arr[j] > x) {
                 arr[j+1] = arr[j];
                 j--;
